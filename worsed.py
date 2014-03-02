5a6d9162af42784a844b9e0eb4959c92d8d0c3c7
cartisan <leon@die-genossen.de>
2014-03-02 01:22:56 +0100

    finished context vectors

--- worsed.py
+++ worsed.py
@@ -3,15 +3,21 @@ from collections import Counter
 from nltk.corpus import stopwords, brown
 from nltk.probability import FreqDist
 from nltk import ConcordanceIndex
-from numpy import array
+from numpy import array, zeros
+
+
+# TODO: lemmatization?
+# TODO: normalization of word-vectors in context-vector creation?
 
 # constants
 dim_num = 2
 feat_num = 7
 window_radius = 3  # how many words to include at each side of occurance
+ambigous_words = ['cat']
 
 # containers
-word_vectors = {}  # maps from features words to lists containing their vectors
+word_vectors = {}  # maps from feature-words to lists containing their vectors
+context_vectors = {}  # maps from ambiguous words to lists of context vectors
 
 test_corpus = ['"', 'But', 'I', "don't", 'want', 'to', 'go', 'among', 'mad', 'people', ',', '"', 'Alice', 'remarked', '.', 'Oh', ',',
 'you', "can't", 'help', 'that', ',', 'said', 'the', 'Cat', ':', "'we're", 'all', 'mad', 'here', '.', "I'm", 'mad', '.', "You're", 'mad', '.',
@@ -63,7 +69,24 @@ def word_vector_from_context(context, dimensions):
     return word_vector
 
 
-def train(corpus):
+def context_vector_from_context(context, word_vectors):
+    """ Takes a list of words as context and all the word vectors.
+    Returns a context vector that is the centroid of the word vectors
+    that occured in the context. Thus gives us second-order co-occurence
+    with the dimension words.
+    """
+
+    dim_num = len(word_vectors.values()[0])
+    centroid = zeros(dim_num, int)
+
+    for word in context:
+        if word in word_vectors:
+            centroid += array(word_vectors[word])
+
+    return centroid
+
+
+def train_sec_order(corpus):
     # remove stop words and signs
     filtered = cleanse_corpus(corpus)
 
@@ -71,7 +94,6 @@ def train(corpus):
     words_desc = FreqDist(filtered).keys()
     dimensions = words_desc[:dim_num]
     features = words_desc[:feat_num]
-
 
     # create word vectors for features
     offset_index = ConcordanceIndex(filtered, key=lambda s: s.lower())
@@ -88,7 +110,15 @@ def train(corpus):
         word_vectors[word] = word_vector
 
     # create context vectors for ambigous words
+    for word in ambigous_words:
+        vectors = []
+        offsets = offset_index.offsets(word)
+        for offset in offsets:
+            context = sized_context(offset, window_radius, filtered)
+            vectors.append(context_vector_from_context(context, word_vectors))
+        context_vectors[word] = vectors
+
     # create sense vectors for ambigous context vectors
 
 
-train(test_corpus)
+train_sec_order(test_corpus)
