ec4b51085cab288885a22201c9c250fddf71cb57
cartisan <leon@die-genossen.de>
2014-03-04 04:30:00 +0100

    combined corpus magic and worsed. giving me trouble in the linear algebra every now and then

--- worsed.py
+++ worsed.py
@@ -1,3 +1,4 @@
+import logging
 import string
 from collections import Counter
 from nltk.corpus import stopwords
@@ -5,9 +6,14 @@ from nltk.probability import FreqDist
 from nltk import ConcordanceIndex
 from numpy import array, zeros, vstack, linspace, diag, dot
 from numpy import sum as npsum
-from numpy.linalg import svd
+from numpy.linalg import svd, norm
 from scipy.cluster.vq import kmeans2
 from scipy.spatial.distance import cosine
+
+from senseval_adapter import split_corpus
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(message)s')
 
 # TODO: lemmatization?
 # TODO: normalization of word-vectors in context-vector creation?
@@ -18,16 +24,16 @@ dim_num = 2  # number of dimensions of word space
 svd_dim_num = 2  # number of dimensions in svd-space
 window_radius = 3  # how many words to include on each side of occurance
 cluster_num = 2
-ambiguous_words = ['mad']
+ambiguous_words = ['hard', 'line', 'serve']
 
 
 
-train_corpus = ['"', 'But', 'I', "don't", 'want', 'to', 'go', 'among', 'mad', 'people', ',', '"', 'Alice', 'remarked', '.', 'Oh', ',',
-'you', "can't", 'help', 'that', ',', 'said', 'the', 'Cat', ':', "'we're", 'all', 'mad', 'here', '.', "I'm", 'mad', '.', "You're", 'mad', '.',
-'How', 'do', 'you', 'know', "I'm", 'mad', '?', 'said', 'Alice', '.', 'You', 'must', 'be', ',', 'said', 'the', 'Cat', ',', 'or', 'you',
-"wouldn't", 'have', 'come', 'here', '.']
-
-test_corpus = train_corpus
+#train_corpus = ['"', 'But', 'I', "don't", 'want', 'to', 'go', 'among', 'mad', 'people', ',', '"', 'Alice', 'remarked', '.', 'Oh', ',',
+#'you', "can't", 'help', 'that', ',', 'said', 'the', 'Cat', ':', "'we're", 'all', 'mad', 'here', '.', "I'm", 'mad', '.', "You're", 'mad', '.',
+#'How', 'do', 'you', 'know', "I'm", 'mad', '?', 'said', 'Alice', '.', 'You', 'must', 'be', ',', 'said', 'the', 'Cat', ',', 'or', 'you',
+#"wouldn't", 'have', 'come', 'here', '.']
+#
+#test_corpus = train_corpus
 
 
 def draw_word_senses(sense_vectors, context_vectors, labels):
@@ -64,7 +70,8 @@ def cleanse_corpus(corpus):
 
     return [word.lower() for word in corpus if
             word.lower() not in stopwords.words('english') and
-            word not in string.punctuation]
+            word not in string.punctuation and
+            len(word) > 2]
 
 
 def sized_context(word_index, window_radius, corpus):
@@ -123,10 +130,10 @@ def svd_reduced_eigenvectors(matrix, dim):
     left eigenvector reduced to dim dimensions.
     """
 
-    if matrix.shape[0] < matrix.shape[1]:
-        raise ValueError("Matrix has more columns (dimensions) than rows (feature vectors).")
-    if len(matrix[0]) < dim:
-        raise ValueError('Reduction to more dimensions than contained in matrix not possible.')
+#    if matrix.shape[0] < matrix.shape[1]:
+#        raise ValueError("Matrix has more columns (dimensions) than rows (feature vectors).")
+#    if len(matrix[0]) < dim:
+#        raise ValueError('Reduction to more dimensions than contained in matrix not possible.')
 
     U, _, _ = svd(matrix, True)
     return U[:, :dim]
@@ -151,12 +158,15 @@ def assign_sense(context_vector, sense_vectors):
 
     cos_similarities = []
     for sense in sense_vectors:
+        if norm(sense) == 0:
+            continue
         cos_similarities.append(cosine(context_vector, sense))
 
     return cos_similarities.index(min(cos_similarities))
 
 
 def train_sec_order(corpus, ambigous_words):
+    logging.info("Start train second order co-occurence")
     # containers
     word_vectors = {}  # maps feature-words to lists containing their vectors
     sense_vectors = {}  # maps ambiguous words to ndarray of sense vectors
@@ -198,29 +208,42 @@ def train_sec_order(corpus, ambigous_words):
         # create sense vectors for ambigous context vectors
         svd_centroids, labels = kmeans2(svd_matrix, cluster_num)
 
+        #draw_word_senses(svd_centroids, svd_matrix, labels)
+
+        # labels tell which context belongs to which cluster in svd
+        # space. Compute centroids in word space according to that
         centroids = []
         for i in range(cluster_num):
             cluster_i = [vector for vector, label in\
                          zip(vectors, labels) if label == i]
-            centroids.append(npsum(vstack(cluster_i), 0))
+            try:
+                centroids.append(npsum(vstack(cluster_i), 0))
+            except ValueError:
+                logging.warning("Empty sense vector")
+                centroids.append(zeros(dim_num))
 
         sense_vectors[word] = centroids
 
         #draw_word_senses(svd_centroids, svd_matrix, labels)
         #draw_word_senses(vstack(centroids), context_matrix, labels)
 
+    logging.info("sense vectors:{}".format(sense_vectors))
+    logging.info("word vectors: {}".format(word_vectors.items()))
     return sense_vectors, word_vectors
 
 
 def test_sec_order(corpus, ambiguous_words, sense_vectors, word_vectors):
-    # remove stop words and signs
-    filtered = cleanse_corpus(corpus)
+    logging.info("Start test second order co-occurence")
 
-    # get word positions in text
-    offset_index = ConcordanceIndex(filtered, key=lambda s: s.lower())
-
-    word_context_labels = {}
     for ambiguous_word in ambiguous_words:
+        logging.info("test for: {}".format(ambiguous_word))
+        # remove stop words and signs
+        filtered = cleanse_corpus(corpus[ambiguous_word])
+
+        # get word positions in text
+        offset_index = ConcordanceIndex(filtered, key=lambda s: s.lower())
+
+        word_context_labels = {}
         offsets = offset_index.offsets(ambiguous_word)
 
         # find all contexts for the word and assign them to a sense
@@ -235,5 +258,11 @@ def test_sec_order(corpus, ambiguous_words, sense_vectors, word_vectors):
 
     return word_context_labels
 
+train_corpus, test_corpus, correct_labels = split_corpus()
+
 senses, words = train_sec_order(train_corpus, ambiguous_words)
 labels = test_sec_order(test_corpus, ambiguous_words, senses, words)
+
+print 'labels ', labels
+print 'correct labels ', correct_labels
+
