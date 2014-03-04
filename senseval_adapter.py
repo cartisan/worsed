97c960d84dcac23fbc0c5567de719b9f0d36f587
cartisan <leon@die-genossen.de>
2014-03-04 03:19:22 +0100

    Finished corpus magic

--- senseval_adapter.py
+++ senseval_adapter.py
@@ -1,4 +1,9 @@
 from nltk.corpus import senseval as seval
+from random import sample
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(message)s')
 
 
 def sense_anal(word):
@@ -9,48 +14,67 @@ def sense_anal(word):
     print 'senses: ', set(senses)
     print "sentences: ", len(senses)
 
-    borders = [0]
-    for i in range(1,len(senses)):
+    borders = [(0, senses[0])]
+    for i in range(1, len(senses)):
         if senses[i] != senses[i-1]:
-            borders.append(i)
+            borders.append((i, senses[i]))
     print "borders: ", borders
+
+# Using sense_anal we find the following:
+# borders:  [(0, 'HARD1'), (3455, 'HARD2'), (3957, 'HARD3')]
+# h1: 3455, h2: 502 --> 3957  [0:3957]
+
+# borders:  [(0, 'cord'), (373, 'division'), (747, 'formation'), (1096, 'phone'), (1525, 'product'), (3742, 'text')]
+# phone: 429, product: 2217 --> 2646 [1096:3742]
+
+# borders:  [(0, 'SERVE10'), (1814, 'SERVE12'), (3086, 'SERVE2'), (3939, 'SERVE6')]
+# serve10: 1814, serve12: 1272 --> 3086 [0:3086]
 
 
 def split_corpus():
+
+    logging.info("start corpus")
+    logging.info("restriction starts")
+
+    # restrict corpora to 2 most common senses
+    hard = seval.instances("hard.pos")[0:3957]
+    line = seval.instances("line.pos")[1096:3742]
+    serve = seval.instances("serve.pos")[0:3086]
+
+    logging.info("value setting starts")
+
+    # smallest corpus has 2646 entries, for simplicity we restrict the
+    # num of samples in all corpora to that
+    sample_range = 2646
+    sample_num = 100
+
     train_p, test_p = 0.8, 0.2
     train, test = [], {'hard': [], 'line': [], 'serve': []}
     labels = {'hard': [], 'line': [], 'serve': []}
+    corpora = [hard, line, serve]
+    samples = sample(range(sample_range), sample_num)  # random order for sentences
+    border = int(sample_num * train_p)
 
-    corpora = ['hard.pos', 'line.pos', 'serve.pos']
-    #sen_count = min([len(seval.instances(corp)) for corp in corpora])  # 4146
-    #border = int(sen_count * train_p)
+    logging.info("training samples start")
 
-    sen_count = 6
-    border = 3
-
-    # train is 80% of corpus, ambiguous words alterning
-    for i in range(border):
+    # ambiguous words alterning to prevent skew
+    for i in samples[:border]:
         for corp in corpora:
-            inst = seval.instances(corp)[i]
-            train += [w for (w, _) in inst.context]
+            inst = corp[i]
+            train += [w[0] for w in inst.context if isinstance(w, tuple)]
 
-    for i in range(border, sen_count):
+    logging.info("test samples start")
+
+    for i in samples[border:]:
         for corp in corpora:
-            word = corp.split('.')[0]
-            inst = seval.instances(corp)[i]
-            test[word] += [w for (w, _) in inst.context]
+            inst = corp[i]
+            word = inst.word.split('-')[0]
+            test[word] += [w[0] for w in inst.context if isinstance(w, tuple)]
             labels[word] += inst.senses
 
+    logging.info("end corpus")
+    logging.info("length train: {}, length test: {}, labels/word: {}".
+                 format(len(train), len(test['hard']) + len(test['line'])
+                 + len(test['serve']), len(labels['hard'])))
+
     return train, test, labels
-
-tr, te, la = split_corpus()
-
-#for f in seval.fileids():
-#    border = int(len(seval.instances(f)) * train_p)
-#    for inst in seval.instances(f)[:border]:
-#        train += [w for (w, _) in inst.context]
-#
-#    word = seval.instances[0].word.split('-')[0]
-#    test[word] = []
-#    for inst in seval.instances(f)[border:]:
-#        test[word] += [w for (w, _) in inst.context]
